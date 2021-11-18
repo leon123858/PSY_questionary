8a450aa713edfb24793960de9641316b185f55fa
sonia1013 <soniatoyota5941@gmail.com>
2021-11-18 22:20:34 +0800

    convert to english version except for GQ/

--- PQ_main-en/PQ_main/routes/index.js
+++ PQ_main-en/PQ_main/routes/index.js
@@ -1,16 +1,16 @@
-﻿'use strict';
-var express = require('express');
-const { Get } = require('./GetConst');
+﻿"use strict";
+var express = require("express");
+const { Get } = require("./GetConst");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res) {
-    res.render('EW/EW1');
+router.get("/", function (req, res) {
+  res.render("EW/EW1");
 });
 
 //test
-router.get('/test', function (req, res) {
-    res.render('testBW');
+router.get("/test", function (req, res) {
+  res.render("testBW");
 });
 
 module.exports = router;
