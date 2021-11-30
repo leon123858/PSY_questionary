d9be09b466a8cbb7077865bf7d0f9256312c2478
leon123858 <a0970785699@gmail.com>
2021-11-30 16:57:58 +0800

    edit deploy_main_server.ps1

--- deploy_script/deploy_main_server.ps1
+++ deploy_script/deploy_main_server.ps1
@@ -1,4 +1,5 @@
 Set-Location ..
+docker stop redis
 Write-Host "update to Lastest branch in master"
 git pull
 Write-Host "Stop PM2 service"
@@ -6,4 +7,5 @@ Stop-Service -DisplayName  "PM2" -Force
 pm2 kill
 Write-Host "reStart PM2 service"
 Start-Service -DisplayName  "PM2"
+docker start redis
 Set-Location .\deploy_script
